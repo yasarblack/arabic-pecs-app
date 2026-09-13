80c0548baacadcfddb18973f3f0b9098fe56e69b
yasarblack <ly5222327@gmail.com>
2026-09-14 01:50:17 +0300

    Implement routing in App component

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from 'react-router-dom'
+import Login from './pages/Login.jsx'
+import ProfileSelect from './pages/ProfileSelect.jsx'
+import Board from './pages/Board.jsx'
+import Admin from './pages/Admin.jsx'
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/profiles" element={<ProfileSelect />} />
+      <Route path="/board/:profileId" element={<Board />} />
+      <Route path="/admin" element={<Admin />} />
+    </Routes>
+  )
+}
