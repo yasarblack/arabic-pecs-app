3f754c96ef1ed9acec65ea1bead442d9fa85a58b
yasarblack <ly5222327@gmail.com>
2026-09-14 02:00:45 +0300

    Add Admin route to App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Login from './pages/Login.jsx'
 import ProfileSelect from './pages/ProfileSelect.jsx'
 import Board from './pages/Board.jsx'
+import Admin from './pages/Admin.jsx'
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Route path="/" element={<Login />} />
       <Route path="/profiles" element={<ProfileSelect />} />
       <Route path="/board/:profileId" element={<Board />} />
+      <Route path="/admin" element={<Admin />} />
     </Routes>
   )
 }
