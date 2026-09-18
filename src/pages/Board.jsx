3a6e1cb1fb3aaeba24d9c6e5b4b79b64f54aa2ac
yasarblack <ly5222327@gmail.com>
2026-09-18 23:53:02 +0300

    Update Board.jsx

--- src/pages/Board.jsx
+++ src/pages/Board.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
 import { supabase } from '../lib/supabaseClient'
 import { useNavigate, useParams } from 'react-router-dom'
 export default function Board() {
