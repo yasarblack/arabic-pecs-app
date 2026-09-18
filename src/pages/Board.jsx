0e1f959d11cfa083f0e8771659950c6ebfe9c9f0
yasarblack <ly5222327@gmail.com>
2026-09-18 23:23:55 +0300

    Update Board.jsx

--- src/pages/Board.jsx
+++ src/pages/Board.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { supabase } from '../lib/supabaseClient'
-
+import { useNavigate, useParams } from 'react-router-dom'
 export default function Board() {
   const { profileId } = useParams()
 
