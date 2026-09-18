6107530f5a68045ff218641ad73b278637fcd77d
yasarblack <ly5222327@gmail.com>
2026-09-18 23:20:51 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Login from './pages/Login.jsx'
 import ProfileSelect from './pages/ProfileSelect.jsx'
 import Board from './pages/Board.jsx'
 import Admin from './pages/Admin.jsx'
-
+import SentenceBuilder from './pages/SentenceBuilder.jsx'
 export default function App() {
   return (
     <Routes>
